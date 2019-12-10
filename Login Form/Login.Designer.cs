11240cc3797ffdad502696c5680594c37951fbae
Sanjaya Ekanayake <snjekanayake@gmail.com>
2019-12-10 21:25:52 +0530

    This is a simple login form using c sharp
    Usernames and Passwords for the login are;
    
    Username - Adimin
    Password - admin
    
    Username - Client
    Password - pass

---
+++ Login Form/Login.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Login_Form
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlmove = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtuserName = new System.Windows.Forms.TextBox();
+            this.txtuserPassword = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnclose = new System.Windows.Forms.Button();
+            this.pnlmove.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlmove
+            // 
+            this.pnlmove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(13)))), ((int)(((byte)(22)))));
+            this.pnlmove.Controls.Add(this.panel2);
+            this.pnlmove.Controls.Add(this.label3);
+            this.pnlmove.Controls.Add(this.btnclose);
+            this.pnlmove.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlmove.Location = new System.Drawing.Point(0, 0);
+            this.pnlmove.Name = "pnlmove";
+            this.pnlmove.Size = new System.Drawing.Size(350, 50);
+            this.pnlmove.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.panel2.Location = new System.Drawing.Point(0, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(350, 10);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtuserName
+            // 
+            this.txtuserName.Location = new System.Drawing.Point(156, 74);
+            this.txtuserName.Name = "txtuserName";
+            this.txtuserName.Size = new System.Drawing.Size(154, 22);
+            this.txtuserName.TabIndex = 1;
+            // 
+            // txtuserPassword
+            // 
+            this.txtuserPassword.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtuserPassword.Location = new System.Drawing.Point(156, 117);
+            this.txtuserPassword.Name = "txtuserPassword";
+            this.txtuserPassword.Size = new System.Drawing.Size(154, 22);
+            this.txtuserPassword.TabIndex = 2;
+            this.txtuserPassword.UseSystemPasswordChar = true;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlogin.ForeColor = System.Drawing.Color.Yellow;
+            this.btnlogin.Location = new System.Drawing.Point(252, 234);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 29);
+            this.btnlogin.TabIndex = 3;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Location = new System.Drawing.Point(16, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Location = new System.Drawing.Point(16, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Location = new System.Drawing.Point(3, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Simple Login Form";
+            // 
+            // btnclose
+            // 
+            this.btnclose.FlatAppearance.BorderSize = 0;
+            this.btnclose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnclose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnclose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnclose.ForeColor = System.Drawing.Color.Yellow;
+            this.btnclose.Location = new System.Drawing.Point(300, 15);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(38, 32);
+            this.btnclose.TabIndex = 3;
+            this.btnclose.Text = "X";
+            this.btnclose.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(41)))), ((int)(((byte)(68)))));
+            this.ClientSize = new System.Drawing.Size(350, 292);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.txtuserPassword);
+            this.Controls.Add(this.txtuserName);
+            this.Controls.Add(this.pnlmove);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.pnlmove.ResumeLayout(false);
+            this.pnlmove.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlmove;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtuserName;
+        private System.Windows.Forms.TextBox txtuserPassword;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnclose;
+    }
+}
+
---
+++ Login Form/Login.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login_Form
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=F:\C# Projects\Login Form\Login Form\loginDatabase.mdf;Integrated Security=True");
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM Authentication WHERE userName='" + txtuserName.Text + "'AND password='" + txtuserPassword.Text + "' ", conn);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                if (dt.Rows.Count == 1)
+                {
+                    MainForm mf = new MainForm();
+                    mf.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password is Incorrect");
+                }
+            }
+            catch(Exception e01)
+            {
+                MessageBox.Show("ERROR" + e01);
+            }
+        }
+
+    }
+}
